8bded9be4f7326d36121046a72b3359420e8f0b7
sist-116 <sist-116@DESKTOP-EQAULAU>
2024-04-11 12:19:02 +0900

    1234

--- firstProject/src/MainClass.java
+++ firstProject/src/MainClass.java
@@ -2,9 +2,8 @@
 public class MainClass {
 
 	public static void main(String[] args) {
-		
 		for(int i = 1 ; i <= 10 ; i++)
-			System.out.printf("Hello java %d번째\n", i);
+			System.out.printf("Hello java %d번째입니다\n", i);
 		
 	}
 
